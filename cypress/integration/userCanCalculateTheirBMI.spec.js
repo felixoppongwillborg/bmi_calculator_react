bd04b908c08c06f0b9682a80c3aa0ad38c7facb5
felixoppongwillborg <felixwillborg@MacBook-Pro.local>
2020-06-29 17:21:43 +0200

    added helpers and wrote implementation code for the BMI message

--- cypress/integration/userCanCalculateTheirBMI.spec.js
+++ cypress/integration/userCanCalculateTheirBMI.spec.js
@@ -4,6 +4,6 @@ describe('BMI Calculator', () => {
         cy.get('input#weight').type(90);
         cy.get('input#height').type(190);
         cy.get('button#calculate').click();
-        cy.get('p#bmi-message').should('contain', 'you are Normal with a BMI of 24.93')
+        cy.get('p#bmi-message').should('contain', 'You are Normal with a BMI of 24.93')
     })
 })
